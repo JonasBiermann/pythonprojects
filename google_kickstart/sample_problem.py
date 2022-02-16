ef7903cbfc57c47cafe1c7277ba05092bce40555
jonasmac <jojabiermann@gmail.com>
2022-02-16 23:34:59 +0100

    added input from terminal but still getting runtime error in google kickstart

--- google_kickstart/sample_problem.py
+++ google_kickstart/sample_problem.py
@@ -1,37 +1,29 @@
-textFile = """2
-7 3
-1 2 3 4 5 6 7
-5 10
-7 7 7 7 7
-"""
-lines = []
-for line in textFile.splitlines():
-    lines.append(line.split())
-cases = int(lines[0][0])
-nM = []
 C = []
-x = 1
-for i in range(cases):
+NM = []
+t = int(input())
+for i in range(t):
+    nM = []
+    n, m = [int(s) for s in input().split(" ")]
+    print(n, m)
+    nM.extend([n, m])
+    for j in range(t-1):
+        c = input().split(" ")
+        print(c)
+        C.append(c)
+    NM.append(nM)
+x = 0
+print(NM)
+bagCon = []
+for i in range(t):
     caseList = []
-    for j in range(cases):
-        caseList.append((int(lines[x][j])))
-    nM.append(caseList)
-    x = 3
-i = 0
-j = 0
-x = 2
-for i in range(cases):
-    caseList = []
-    for j in range(nM[i][0]):
-        caseList.append(int(lines[x][j]))
-    C.append(caseList)
-    x = 4
-i = 0
-j = 0
+    for j in range(NM[i][0]):
+        caseList.append(int(C[i][j]))
+    bagCon.append(caseList)
+print(bagCon)
 amount = 0
-for i in range(cases):
-    for j in range(nM[i][0]):
-        amount += C[i][j]
-    cPK = amount % nM[i][1]
-    print('Case #', i+1,': ', cPK)
+for i in range(t):
+    for j in range(NM[i][0]):
+        amount += bagCon[i][j]
+    cPK = amount % NM[i][1]
+    print('Case #',i+1,':', cPK)
     amount = 0
